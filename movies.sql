621775884bc09d0d38519bd424711422e0a5e9b2
oogiepanda <harlan.oogie@gmail.com>
2020-06-12 18:13:40 -0700

    updated movie.sql more

--- movies.sql
+++ movies.sql
@@ -5,5 +5,5 @@ use movies;
 create table credentials (id int not null auto_increment, username varchar(6) not null, 
 password varchar(6) not null, affiliation int, primary key (id));
 
-create table credentials (id int not null auto_increment, moviename varchar(35) not null, 
+create table movielist (id int not null auto_increment, moviename varchar(35) not null, 
 director varchar(20) not null, year int, genre varchar(10), primary key (id));
